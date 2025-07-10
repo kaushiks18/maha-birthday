dff07f06f1e2cda89cc02b48f3c9d741fa071d59
sKaushik <sec23it068@sairamtap.edu.in>
2025-07-10 18:51:23 +0530

    Initial commit: Birthday site with GitHub Pages deployment

---
+++ src/data.js
@@ -0,0 +1,33 @@
+// Your Anniversary Date (Format: yyyy-mm-ddTHH:MM:SS)
+export const BIRTHDAY_DATE = "2025-07-11T00:00:00"; // Kartik insta @kartikk_sahh
+
+// A sweet short message shown below the Happy Anniversary heading
+export const topMessage = "HAPPY BIRTHDAY SHAGUN!❤️🥹 aaj ka din aapka h sirf or sirf aapka aap har din khushiyan deserve krti ho lekin aaj kuch special toh hona hi chahiye or dino se ab hum saath nhi h toh syd na kr paun kuch cheezein jo soochi thi but kya kr sakte, jitna efforts aapne mere birthday pr kare the uske .1% bhi nhi kr skta m sorry uske liye😔 BUT I LOVE YOU SO MUCHHH💗🫶🏻 Lekin hmesha yeii dua rhegi bhagwan se aap khush raho hmesha hmesha kyuki aap deserve krti ho hr vo cheez jo aapko khush krti ho jisse aapko khushi milti ho❤️❤️ HOPE YOU RECEIVE ALL THE LOVE, HAPPINESS AND CARE YOU DESERVE🥹🧿🧿"
+
+// These are the photos shown in the gallery section.
+// Add your images in the "public" folder.
+// Then write the file name here like: "/img1.jpg"
+// You can change the alt text to describe the photo.
+// To add more photos, just copy and paste the same line below.
+export const photos = [
+  { src: "/img1.jpg" },
+  { src: "/img2.jpg" },
+  { src: "/img3.jpg" },
+  { src: "/img4.jpg" },
+  { src: "/img5.jpg" },
+];
+
+// The full letter message, change it accordingly
+export const letterMessage = `Dear GORI💕,
+HAPPIEST BIRTHDAY MY LOVE💖
+Hope, Aapka ye din sabse special ho hona bhi chahiye kyuki aap khud special ho sabse best meri jindagi ki sbse special person ho aap🫶🏻❤️ Vo insaan ho jisne mujhe meri life ke sabse best moments diye hain THANKUU BABY❤️❤️
+ONCE AGAIN HAPPIEST BIRTHDAY MERI JAAN💗💘🫶🏻
+I HOPE YOUR BIRTHDAY IS FULL OF LOVE AND EVERYTHING THAT MAKES YOU HAPPY AND SMILE🥰🥹
+I LOVE YOU SO MUCH❤️🥹🫶🏻`;
+
+// Name shown in the footer, change it to yours
+export const authorName = "Kartik"
+
+// Background music file path
+// Place your .mp3 file inside the public folder and give the path like below
+export const audioSrc = "./bg.mp3"
