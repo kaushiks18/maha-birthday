4e1e87da8445850d49d7e4c6e2411e61629d43b1
kaushik.S <sec23it068@sairamtap.edu.in>
2025-07-10 22:52:25 +0530

    data.js

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 // Your Anniversary Date (Format: yyyy-mm-ddTHH:MM:SS)
-export const BIRTHDAY_DATE = "2025-07-11T00:00:00"; // Kartik insta @kartikk_sahh
+export const BIRTHDAY_DATE = "2025-07-11T00:00:00"; 
 
 // A sweet short message shown below the Happy Anniversary heading
-export const topMessage = "HAPPY BIRTHDAY SHAGUN!❤️🥹 aaj ka din aapka h sirf or sirf aapka aap har din khushiyan deserve krti ho lekin aaj kuch special toh hona hi chahiye or dino se ab hum saath nhi h toh syd na kr paun kuch cheezein jo soochi thi but kya kr sakte, jitna efforts aapne mere birthday pr kare the uske .1% bhi nhi kr skta m sorry uske liye😔 BUT I LOVE YOU SO MUCHHH💗🫶🏻 Lekin hmesha yeii dua rhegi bhagwan se aap khush raho hmesha hmesha kyuki aap deserve krti ho hr vo cheez jo aapko khush krti ho jisse aapko khushi milti ho❤️❤️ HOPE YOU RECEIVE ALL THE LOVE, HAPPINESS AND CARE YOU DESERVE🥹🧿🧿"
+export const topMessage = "HAPPY BIRTHDAY MAHA!❤️🥹 aaj ka din aapka h sirf or sirf aapka aap har din khushiyan deserve krti ho lekin aaj kuch special toh hona hi chahiye or dino se ab hum saath nhi h toh syd na kr paun kuch cheezein jo soochi thi but kya kr sakte, jitna efforts aapne mere birthday pr kare the uske .1% bhi nhi kr skta m sorry uske liye😔HOPE YOU RECEIVE ALL THE LOVE, HAPPINESS AND CARE YOU DESERVE🥹🧿🧿"
 
 // These are the photos shown in the gallery section.
 // Add your images in the "public" folder.
@@ -18,15 +18,15 @@ export const photos = [
 ];
 
 // The full letter message, change it accordingly
-export const letterMessage = `Dear GORI💕,
-HAPPIEST BIRTHDAY MY LOVE💖
+export const letterMessage = `Dear Milleur ami💕,
+HAPPIEST BIRTHDAY MY MAHA💖🦋
 Hope, Aapka ye din sabse special ho hona bhi chahiye kyuki aap khud special ho sabse best meri jindagi ki sbse special person ho aap🫶🏻❤️ Vo insaan ho jisne mujhe meri life ke sabse best moments diye hain THANKUU BABY❤️❤️
 ONCE AGAIN HAPPIEST BIRTHDAY MERI JAAN💗💘🫶🏻
 I HOPE YOUR BIRTHDAY IS FULL OF LOVE AND EVERYTHING THAT MAKES YOU HAPPY AND SMILE🥰🥹
 I LOVE YOU SO MUCH❤️🥹🫶🏻`;
 
 // Name shown in the footer, change it to yours
-export const authorName = "Kartik"
+export const authorName = "Maha....🦋🤎"
 
 // Background music file path
 // Place your .mp3 file inside the public folder and give the path like below
